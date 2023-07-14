9e22b2af9200ee4325152f0a7d400af60c442204
Gavin Coleman <gavinjcoleman@gmail.com>
2023-07-14 20:26:47 +0100

    ok at 12:45

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -20,8 +20,36 @@ const CartScreen = () => {
             Your cart is empty <Link to='/'>Go Back</Link>
           </Message>
         ) : (
-          <ListGroup variant='flush'>Items</ListGroup>
-            
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded/>
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>
+                    ${item.price}
+                  </Col>
+                  <Col md={2}>
+                  <Form.Control
+                        as='select'
+                        value={item.qty}
+                        onChange={(e) => {}}
+                        >
+                          {[...Array(item.countInStock).keys()].map((x) => (
+                            <option key={ x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                      </Form.Control>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
         )}
       </Col>
     </Row>
