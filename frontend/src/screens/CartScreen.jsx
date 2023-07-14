b83a925ac062d7e5c22cfaaa24647f77e86202bf
Gavin Coleman <gavinjcoleman@gmail.com>
2023-07-14 19:54:51 +0100

    ok at 07:00

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -7,9 +7,24 @@ import Message from '../components/Message'
 const CartScreen = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  
+
+  const cart = useSelector((state) => state.cart)
+  const {cartItems} = cart
+
   return (
-    <div>CartScreen</div>
+    <Row>
+      <Col md={8}>
+        <h1 style={{marginBottom: '20px' }}>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>Items</ListGroup>
+            
+        )}
+      </Col>
+    </Row>
   )
 }
 
