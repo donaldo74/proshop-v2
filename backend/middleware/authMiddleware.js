deb8f8f178dcff8fd2f0535d0b411982c4107322
Gavin Coleman <gavinjcoleman@gmail.com>
2023-07-17 21:10:39 +0100

    All working fine

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import asyncHandler from './asyncHandler.js'
 import User from '../models/userModel.js'
 
 // Protect routes
-export const protect = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
   let token;
 
   // Read the JWT from the cookie
@@ -12,12 +12,27 @@ export const protect = asyncHandler(async (req, res, next) => {
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      req.user = await User.findById(decoded.userId.select('-password'))
+      req.user = await User.findById(decoded.userId).select('-password')
+      next()
     } catch (error) {
-
+      console.log(error)
+      res.status(401)
+      throw new Error('Not authorized,token failed')
     }
   } else {
     res.status(401)
     throw new Error('Not authorized, no token')
   }
 })
+
+// Admin Middleware
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorized as admin')
+  }
+}
+
+export {protect, admin}
