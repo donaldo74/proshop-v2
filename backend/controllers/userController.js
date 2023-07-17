19ac40f7b2807f144c85f0a0bbfe22e4c7a27639
Gavin Coleman <gavinjcoleman@gmail.com>
2023-07-17 22:09:58 +0100

    Working fine

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js'
 import User from '../models/userModel.js'
-import jwt from 'jsonwebtoken'
+import generateToken from '../utils/generateToken.js'
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -11,15 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({email})
 
   if(user && (await user.matchPassword(password))) {
-    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET,{expiresIn: '30d'})
-
-    // Set JWT as HTTP-Only cookie on the server
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000 // 30 Days
-    })
+    generateToken(res, user._id)
 
     res.json({
       _id: user._id,
@@ -37,7 +29,36 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user')
+  // Get name, email & password from the req body
+  const {name, email, password} = req.body
+  
+  // Check if the user already exists
+  const userExists = await User.findOne({email})
+
+  if(userExists) {
+    res.status(400) // Client error
+    throw new Error('User already exists')
+  }
+
+  // If user doesnt exist, create it
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if (user) {
+    generateToken(res, user._id)
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @desc    Logout user / clear cookie
