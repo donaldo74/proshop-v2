4c6ba164bdfd27eb534555c3310d7f057d8890b4
Gavin Coleman <gavinjcoleman@gmail.com>
2023-07-26 21:49:06 +0100

    Super

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -26,12 +26,17 @@ const cartSlice = createSlice({
 
       return updateCart(state)
     },
-    saveShippingAddress: (state, action) => {state.shippingAddress = action.payload
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload
+      return updateCart(state)
+    },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload
       return updateCart(state)
     }
   },
 })
 
-export const {addToCart, removeFromCart, saveShippingAddress} = cartSlice.actions
+export const {addToCart, removeFromCart, saveShippingAddress, savePaymentMethod} = cartSlice.actions
 
 export default cartSlice.reducer
