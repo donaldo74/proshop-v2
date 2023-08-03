331ba56e57ca5508592ac2406bb4cb4192df8f83
Gavin Coleman <gavinjcoleman@gmail.com>
2023-08-03 20:12:16 +0100

    PayPal on backend looking ok

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,38 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-import cookieParser from 'cookie-parser'
-import connectDB from './config/db.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-const port = process.env.PORT || 5000
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+const port = process.env.PORT || 5000;
 
-connectDB()  // Connect to MongoDB
+connectDB(); // Connect to MongoDB
 
-const app = express()
+const app = express();
 
 // Body parser middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser middleware
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.get('/', (req,res) => {
-  res.send('API is running...')
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
