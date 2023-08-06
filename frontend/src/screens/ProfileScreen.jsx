346c2edb6439e0b97ff267001c29de29f3ace404
Gavin Coleman <gavinjcoleman@gmail.com>
2023-08-06 12:56:10 +0100

    Ok to 3:50 on video

---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react"
+import {Table, Form, Button, Row, Col} from "react-bootstrap"
+import { LinkContainer } from "react-router-bootstrap"
+import {useDispatch, useSelector} from "react-redux"
+import { toast } from "react-toastify"
+import Message from "../components/Message"
+import Loader from "../components/Loader"
+import { useProfileMutation } from "../slices/usersApiSlice"
+import { setCredentials } from "../slices/authSlice"
+
+
+const ProfileScreen = () => {
+  return (
+    <div>ProfileScreen</div>
+  )
+}
+
+export default ProfileScreen
