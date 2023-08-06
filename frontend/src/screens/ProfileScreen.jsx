636eb6cf797d88799bad34499fd5ad7969974e98
Gavin Coleman <gavinjcoleman@gmail.com>
2023-08-06 23:00:43 +0100

    Looks ok to 10:30 on video

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -10,8 +10,82 @@ import { setCredentials } from "../slices/authSlice"
 
 
 const ProfileScreen = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const dispatch = useDispatch()
+
+  const { userInfo} = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(userInfo) {
+      setName(userInfo.name)
+      setEmail(userInfo.email)
+    }
+  }, [userInfo.name, userInfo.email])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log('submitHandler')
+  }
+
   return (
-    <div>ProfileScreen</div>
+    <Row>
+      <Col md={3}>
+        <h2>User Profile</h2>
+
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='name' className="my-2">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type='name'
+              placeholder='Enter name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='email' className="my-2">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='password' className="my-2">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId='confirmpassword' className="my-2">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Confirm password'
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button type='submit' variant='primary' className="my-2">
+            Update
+          </Button>
+        </Form>
+      </Col>
+      <Col md={9}>
+        Column 2
+      </Col>
+    </Row>
   )
 }
 
