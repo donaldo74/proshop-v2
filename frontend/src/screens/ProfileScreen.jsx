fee9adcf20fb5026e5b4bcc10a5e3dc52997c32d
Gavin Coleman <gavinjcoleman@gmail.com>
2023-08-07 20:24:09 +0100

    A thing of beauty

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -19,16 +19,28 @@ const ProfileScreen = () => {
 
   const { userInfo} = useSelector((state) => state.auth)
 
+  const [updateProfile, { isLoading:loadingUpdateProfile}] = useProfileMutation()
+
   useEffect(() => {
     if(userInfo) {
       setName(userInfo.name)
       setEmail(userInfo.email)
     }
-  }, [userInfo.name, userInfo.email])
+  }, [userInfo, userInfo.name, userInfo.email])
 
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault()
-    console.log('submitHandler')
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match')
+    } else {
+      try {
+        const res = await updateProfile({ _id:userInfo._id, name, email, password}).unwrap()
+        dispatch(setCredentials(res))
+        toast.success('Profile updated successfully')
+      } catch (err) {
+        toast.error(err?.data?.message || err.error)
+      }
+    }
   }
 
   return (
@@ -80,6 +92,7 @@ const ProfileScreen = () => {
           <Button type='submit' variant='primary' className="my-2">
             Update
           </Button>
+          { loadingUpdateProfile && <Loader />}
         </Form>
       </Col>
       <Col md={9}>
