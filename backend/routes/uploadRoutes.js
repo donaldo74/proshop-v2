afa4b7d62df237976b6b573d6c361a49e6b06c18
Gavin Coleman <gavinjcoleman@gmail.com>
2023-08-22 21:37:48 +0100

    Image Update now working

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -33,13 +33,11 @@ const upload = multer({
   storage,
 });
 
-router.post(
-  "/",
-  upload.single("image", (req, res) => {
-    res.send({
-      message: "Image Uploaded",
-      image: `/${req.file.path}`,
-    });
-  })
-);
+router.post("/", upload.single("image"), (req, res) => {
+  res.send({
+    message: "Image Uploaded",
+    image: `/${req.file.path}`,
+  });
+});
+
 export default router;
