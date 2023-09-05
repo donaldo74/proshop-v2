285458def09c7f6277518768ec22b3e1736ab45f
Gavin Coleman <gavinjcoleman@gmail.com>
2023-09-05 12:50:39 +0100

    Working fine

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -31,6 +31,22 @@ const ProductScreen = () => {
     navigate('/cart')
   }
 
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    try {
+      await createReview({
+        productId,
+        rating,
+        comment,
+      }).unwrap()
+      refetch()
+      toast.success('Review Submitted')
+      setRating(0)
+      setComment('')
+    } catch (err) {
+      toast.error(err?.data?.message || err.error)
+    }
+  }
   return (
     <>
       <Link className="btn btn-light my-3" to="/">Go Back</Link>
@@ -124,6 +140,49 @@ const ProductScreen = () => {
                 <p>{review.comment}</p>
               </ListGroup.Item>
             ))}
+              <ListGroup.Item>
+                <h2>Write a Customer Review</h2>
+                
+                {loadingProductReview && <Loader />}
+
+                {userInfo ? (
+                  <Form onSubmit={ submitHandler }>
+                    <Form.Group controlId='rating' className='my-2'>
+                      <Form.Label>Rating</Form.Label>
+                      <Form.Control
+                        as='select'
+                        value={rating}
+                        onChange={(e) => setRating(Number(e.target.value))}
+                      >
+                        <option value=''>Select...</option>
+                        <option value='1'>1 - Poor</option>
+                        <option value='2'>2 - Fair</option>
+                        <option value='3'>3 - Good</option>
+                        <option value='4'>4 - Very Good</option>
+                        <option value='5'>5 - Excellent</option>
+                      </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='comment' className='my-2'>
+                      <Form.Label>Comment</Form.Label>
+                      <Form.Control
+                        as='textarea'
+                        row='3'
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                      ></Form.Control>
+                    </Form.Group>
+                    <Button
+                      disabled={loadingProductReview}
+                      type='submit'
+                      variant='primary'
+                    >Submit</Button>
+                  </Form>
+                ) : (
+                  <Message>
+                    Please <Link to='/login'>sign in</Link> to write a review{''}
+                  </Message>
+                )}
+              </ListGroup.Item>
           </ListGroup>
         </Col>
       </Row>
